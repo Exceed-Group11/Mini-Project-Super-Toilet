a3fed94967f39e193615ae36069a0a1d03a54639
Siratee K <sirateek@gmail.com>
2022-02-12 10:55:57 +0700

    Init the project

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+
+app = FastAPI()
+mongo_client = MongoClient('mongodb://localhost', 27017)
