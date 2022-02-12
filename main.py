7fbd142c3e2d3942729145e02ad5703f9513a4a1
Siratee K <sirateek@gmail.com>
2022-02-12 12:10:48 +0700

    Remove the { at the beginnig of the Get Toilet Status API and add some todos

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ mongo_client = MongoClient('mongodb://localhost', 27017)
 db = mongo_client["SuperToilet"]
 collection = db["Toilets"]
 
-@app.get("{/toilet/{toilet_id}")
-def show_status(toilet_id:int ):
-    check = collection.find({"toilet_id":toiled_id}, {"_id":0})
-    return check
+
+@app.get("/toilet/{toilet_id}")
+def show_status(toilet_id: int):
+    check = collection.find({"toilet_id": toilet_id}, {
+                            "_id": 0, "toilet_id": 0})
+    list_check = list(check)
+    # TODO: Check if the toilet_id is found.
+    return list_check.pop()
